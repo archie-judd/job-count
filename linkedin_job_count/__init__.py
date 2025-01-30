7208daa1a14ba97573925c0b5a089f534a3bba21
archiejudd <archie.judd@yahoo.com>
2025-01-30 16:12:33 +0000

    switch to uv!

---
+++ linkedin_job_count/__init__.py
@@ -0,0 +1,2 @@
+__project_name__ = "linkedin-job-count"
+__version__ = "0.0.1"
--- linkedin_job_count/main.py
+++ linkedin_job_count/main.py
@@ -1,8 +1,10 @@
+import argparse
 import csv
 import datetime as dt
 import logging
 import os
 import re
+from argparse import ArgumentParser
 from pathlib import Path
 
 from pydantic import BaseModel
@@ -13,6 +15,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+
+from linkedin_job_count import __project_name__, __version__
 
 logger = logging.getLogger(__name__)
 
@@ -30,8 +34,6 @@ class Job(BaseModel):
 class JobWithCount(Job):
     ts: dt.datetime
     count: int
-    job_title: str
-    location: str
 
 
 LINKEDIN_BASE_URL = "https://www.linkedin.com"
@@ -42,9 +44,10 @@ def read_jobs_to_search_for(file_path: Path) -> list[Job]:
         return [Job.model_validate(row) for row in csv.DictReader(file)]
 
 
-def write_results(file_path: Path, data: list[dict]):
+def write_jobs_with_counts(file_path: Path, jobs_with_counts: list[JobWithCount]):
     file_exists = os.path.exists(file_path) and os.path.getsize(file_path) > 0
 
+    data = [job.model_dump() for job in jobs_with_counts]
     with open(file_path, mode="a", newline="", encoding="utf-8") as file:
         writer = csv.DictWriter(file, fieldnames=data[0].keys())
 
@@ -79,8 +82,6 @@ def login_to_linkedin(driver: WebDriver, email: str, password: str):
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "global-nav"))
     )
-
-    logger.info("Successfully logged in!")
 
 
 def get_job_count(driver: WebDriver, job_title: str, location: str) -> int:
@@ -118,6 +119,95 @@ def get_job_count(driver: WebDriver, job_title: str, location: str) -> int:
     return count
 
 
+def setup_parser() -> ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog=__project_name__,
+        description=("A CLI tool for counting job postings on LinkedIn."),
+        add_help=True,
+    )
+    parser.add_argument(
+        "--version", "-V", action="version", version=f"{__project_name__} {__version__}"
+    )
+
+    parser.add_argument(
+        "input-file",
+        metavar="input-file",
+        help=(
+            "The ID of the Spotify playlist on which to add the BBC station's current"
+            " playlist tracks."
+        ),
+        type=str,
+    )
+    parser.add_argument(
+        "output-file",
+        help=(""),
+        metavar="output-file",
+        type=str,
+    )
+    parser.add_argument(
+        "--chrome-user-data-dir",
+        "-d",
+        help=(
+            "Path to your chrome user data directory, for storing cookies (to prevent needing to login each time)."
+        ),
+        required=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--email",
+        "-e",
+        help=(
+            "Your LinkedIn email address. If not provided, the tool will look for the "
+            "environment variable LINKEDIN_EMAIL"
+        ),
+        required=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--password",
+        "-p",
+        help=(
+            "Your LinkedIn email address. If not provided, the tool will look for the "
+            "environment variable LINKEDIN_PASSWORD"
+        ),
+        required=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--headed",
+        "-H",
+        help=(
+            "Remove all duplicates and any tracks that are not in the source playlist."
+        ),
+        required=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        help="Increase logging verbosity (-vv to increase further).",
+        action="count",
+        default=0,
+    )
+    parser.add_argument(
+        "--quiet",
+        "-q",
+        help="Decrease logging verbosity (-qq to decrease further).",
+        action="count",
+        default=0,
+    )
+    parser.add_argument(
+        "--log-file",
+        "-l",
+        help="Write logs to this file. Suppresses logging in stdout.",
+        required=False,
+        default=None,
+        type=str,
+    )
+
+    return parser
+
+
 def main():
 
     DATA_DIR = Path(__file__).parent.parent / "data"
@@ -130,16 +220,27 @@ def main():
         datefmt="%Y-%m-%dT%H:%M:%S %Z",
     )
 
+    parser = setup_parser()
+    args = parser.parse_args()
+
     environment = Environment.model_validate(os.environ)
     jobs = read_jobs_to_search_for(INPUT_FILE)
 
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--user-data-dir=data")
     driver = webdriver.Chrome(options=options)
 
-    login_to_linkedin(driver, environment.EMAIL, environment.PASSWORD)
+    driver.get(f"{LINKEDIN_BASE_URL}/feed/")
 
-    job_with_counts: list[JobWithCount] = []
+    if "login" in driver.current_url:
+        logger.info("Logging in")
+        login_to_linkedin(driver, environment.EMAIL, environment.PASSWORD)
+        logger.info("Successfully logged in!")
+    else:
+        logging.info("Already logged in to LinkedIn")
+
+    jobs_with_counts: list[JobWithCount] = []
     for job in jobs:
         count = get_job_count(driver, job.job_title, job.location)
         logging.info(f"Number of {job.job_title} jobs in {job.location}: {count}")
@@ -149,9 +250,10 @@ def main():
             count=count,
             ts=dt.datetime.now(dt.timezone.utc),
         )
-        job_with_counts.append(job_with_count)
+        jobs_with_counts.append(job_with_count)
 
-    data = [job.model_dump() for job in job_with_counts]
-    write_results(RESULTS_FILE, data)
+    write_jobs_with_counts(RESULTS_FILE, jobs_with_counts)
 
     driver.quit()
+
+    logging.info("Done!")
